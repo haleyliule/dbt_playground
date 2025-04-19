41db399b267929538d5b740058227751ca1278c6
H L <haleyliule@gmail.com>
2025-04-18 20:57:21 -0400

    update sql

--- ecom/models/staging/brazil-ecom/stg_order_payments_dataset.sql
+++ ecom/models/staging/brazil-ecom/stg_order_payments_dataset.sql
@@ -1,3 +1,8 @@
 {{ config(schema='bronze')}}
 
-select distinct * from {{ source('mysql_source', 'olist_order_payments_dataset') }}
+with base as (
+select distinct *, max(payment_sequential) over (partition by order_id) as max_seq from {{ source('mysql_source', 'olist_order_payments_dataset') }})
+
+select *,
+case when max_seq >1 then 1 else 0 end as multi_payment_methods 
+from base
